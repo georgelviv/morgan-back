a0748c85cc75d8f71701e132d5149dbe00abd658
Yurii Shkoropad <lvivgeorge@gmail.com>
2026-01-05 22:20:07 +0200

    feat: store image in storage

--- functions/src/generate-image.ts
+++ functions/src/generate-image.ts
@@ -2,6 +2,7 @@ import { GoogleGenAI } from '@google/genai';
 import { onCall } from "firebase-functions/https";
 import * as logger from "firebase-functions/logger";
 import { defineSecret } from 'firebase-functions/params';
+import { getStorage } from 'firebase-admin/storage';
 
 const GEMINI_API_KEY = defineSecret('GEMINI_API_KEY');
 
@@ -12,31 +13,29 @@ export const generateImageFn = onCall(
     secrets: [
       GEMINI_API_KEY
     ]
-  },
-  async () => {
+  }, async () => {
 
   try {
-    const ai = new GoogleGenAI({
-      apiKey: GEMINI_API_KEY.value()
-    });
-    const response = await ai.models.generateImages({
-      model: 'imagen-4.0-generate-001',
-      prompt: 'Funny puppy',
-      config: {
-        numberOfImages: 1,
-      }
-    });
-
-    if (!response.generatedImages) {
-      logger.error('Failed to generate image');
+    const base64Image = await generateImage();
+    if (!base64Image) {
       return {
         success: false
-      }
+      };
     }
-    const generatedImage = response.generatedImages[0].image?.imageBytes;
+    const imageBuffer = Buffer.from(base64Image, 'base64');
+    const filePath = `generated-images/${Date.now()}.png`;
+    const bucket = getStorage().bucket();
+    const file = bucket.file(filePath);
+
+    await file.save(imageBuffer, {
+      metadata: {
+        contentType: 'image/png',
+      },
+      resumable: false,
+    });
 
     return {
-      image: generatedImage,
+      image: filePath,
       success: true
     };
   } catch (err) {
@@ -48,3 +47,24 @@ export const generateImageFn = onCall(
   }
 
 });
+
+async function generateImage(): Promise<string | undefined> {
+  const ai = new GoogleGenAI({
+    apiKey: GEMINI_API_KEY.value()
+  });
+  const response = await ai.models.generateImages({
+    model: 'imagen-4.0-generate-001',
+    prompt: 'Funny puppy',
+    config: {
+      numberOfImages: 1,
+    }
+  });
+
+  if (!response.generatedImages) {
+    logger.error('Failed to generate image');
+    return;
+  }
+  const generatedImageBytes = response.generatedImages[0].image?.imageBytes;
+
+  return generatedImageBytes;
+}
